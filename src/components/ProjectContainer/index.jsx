171c6c85b97190ee644452b4e0f3c19234f0660b
Patrick HENG <hengpatrick.pro@gmail.com>
2016-03-27 22:54:54 +0200

    Responsive - Add swipe event via hammerjs

--- src/components/ProjectContainer/index.jsx
+++ src/components/ProjectContainer/index.jsx
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import Hammer from 'hammerjs';
 
 import States from 'core/States';
 import Emitter from 'core/Emitter';
@@ -22,6 +23,11 @@ class Application extends Component {
 
   }
 
+  componentWillMount() {
+    this.canWheel = true;
+    this.wheelPromise = null;
+  }
+
   componentDidMount() {
 
     this.bind();
@@ -37,16 +43,38 @@ class Application extends Component {
   bind() {
 
     this.onKeyUp = this.onKeyUp.bind(this);
+    this.onWheel = this.onWheel.bind(this);
   }
 
   addListerners() {
 
     document.addEventListener('keyup', this.onKeyUp, false);
+
+    if(States.browserName === "firefox") {
+      document.addEventListener('DOMMouseScroll', this.onWheel, false);
+    } else {
+      document.addEventListener('wheel', this.onWheel, false);
+    }
+
+    this.managerHomepage = new Hammer.Manager(this.base, {
+      recognizers: [
+        [Hammer.Swipe, { direction: Hammer.DIRECTION_HORIZONTAL }]
+      ]
+    });
+
+    this.managerHomepage.on('swipeleft', this.nextProject);
+    this.managerHomepage.on('swiperight', this.previousProject);
   }
 
   removeListerners() {
 
     document.removeEventListener('keyup', this.onKeyUp, false);
+
+    if(States.browserName() === "firefox") {
+      document.removeEventListener('DOMMouseScroll', this.onWheel, false);
+    } else {
+      document.removeEventListener('wheel', this.onWheel, false);
+    }
   }
 
   onKeyUp(ev) {
@@ -54,6 +82,35 @@ class Application extends Component {
       this.nextProject();
     } else if (ev.keyCode === 37) {
       this.previousProject();
+    }
+  }
+
+  onWheel(ev) {
+    ev.preventDefault();
+
+    const deltaY = (ev.wheelDelta) ? ev.deltaY : ev.detail * 200;
+
+    if(deltaY < -50 && this.canWheel) {
+      this.canWheel = false;
+
+      clearTimeout(this.wheelPromise);
+
+      this.wheelPromise = setTimeout(()=>{
+        this.canWheel = true;
+      }, 800);
+
+      this.previousProject();
+    }
+    else if(deltaY > 50 && this.canWheel) {
+      this.canWheel = false;
+
+      clearTimeout(this.wheelPromise);
+
+      this.wheelPromise = setTimeout(()=>{
+        this.canWheel = true;
+      }, 800);
+
+      this.nextProject();
     }
   }
 
